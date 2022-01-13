12c28233f7509cf9ca75c1bef94a0ea33c6d2ebb
PavanRaaj <pavanraajgoud@gmail.com>
2022-01-13 16:28:53 +0530

    Fixed some bugs

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+
+
+function holidayTrip(event){
+    event.preventDefault();
+
+    let trips=localStorage.getItem("LoginDetails");
+
+    if(trips){
+        window.open("./pages/holidaytrip.html");
+    }
+    else{
+        alert("Login to View the trips");
+        window.location.href = "./../pages/userlogin.html"
+    }
+}
+
+function summerTrip(event){
+    event.preventDefault();
+
+    let trips2=localStorage.getItem("LoginDetails");
+
+    if(trips2){
+        window.open("./pages/summertrip.html");
+    }
+
+    else{
+        alert("Login to View the trips");
+        window.location.href = "./../pages/userlogin.html"
+    }
+
+}
+
+function pilgrimageTrip(event){
+    event.preventDefault();
+
+    let trips3=localStorage.getItem("LoginDetails");
+
+    if(trips3){
+        window.open("./pages/Pilgrimagetrip.html");
+    }
+
+    else{
+        alert("Login to View the trips");
+        window.location.href = "./../pages/userlogin.html"
+    }
+}
