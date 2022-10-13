c58893e7f2f1578837a7dd4e50686fe9b447bb81
PavanRaaj <pavanraajgoud@gmail.com>
2022-10-13 15:25:30 +0530

    pushing changes

--- assets/js/userlogin.js
+++ assets/js/userlogin.js
@@ -1,54 +1,48 @@
-let storage = []
+let storage = [];
 
 // will check whether Email ID and Psswords are same and store the logged In user details in local storage
+// user 1 = surya@gmail.com,surya123
+// user 2 = pavan@gmail.com,pavan123
+// user 3 = surya@gmail.com,pavan123
 
 function loginStorage(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let email = document.getElementById("mailId").value;
-    let password = document.getElementById("password").value;
-    let bool = false;
+  let email = document.getElementById("mailId").value;
+  let password = document.getElementById("password").value;
+  let bool = false;
 
-    let array = JSON.parse(localStorage.getItem("User Credentials"));
+  let array = JSON.parse(localStorage.getItem("User Credentials"));
 
-let object={}
+  let object = {};
 
-    for (let i of array) {
-        
-
-        if (i.email_id == email && i.createPassWord == password) {
-            bool = true;
-            object=i
-            break;
-        }
+  for (let i of array) {
+    if (i.email_id == email && i.createPassWord == password) {
+      bool = true;
+      object = i;
+      break;
     }
+  }
 
-    console.log(bool)
+  console.log(bool);
 
-
-
-    if (bool) {
-        localStorage.setItem("LoginDetails", JSON.stringify(object))
-        alert("successfully Logged In.")
-        window.location.href = "../pages/MyAccount.html";
-    }
-    else {
-        alert("Invalid Email_ID or Password.");
-    }
-
-
+  if (bool) {
+    localStorage.setItem("LoginDetails", JSON.stringify(object));
+    alert("successfully Logged In.");
+    window.location.href = "../pages/MyAccount.html";
+  } else {
+    alert("Invalid Email_ID or Password.");
+  }
 }
 
 // will show the password wen box is checked
 
 function showPasswordWhenChecked() {
-    let showPassword = document.getElementById("password");
-   
-    if (showPassword.type === "password") {
-        showPassword.type = "text";
-    }
-    else {
-        showPassword.type = "password";
-    }
-}
+  let showPassword = document.getElementById("password");
 
+  if (showPassword.type === "password") {
+    showPassword.type = "text";
+  } else {
+    showPassword.type = "password";
+  }
+}
